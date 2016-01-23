9e107d0e408cb135d264535c730da9eea3547e2b
BBishop <brad.bishop68@gmail.com>
2016-01-23 11:57:22 -1000

    Adds Assets

---
+++ public/js/assets.js
@@ -0,0 +1,22 @@
+//each asset is organized by type
+//each asset has structure
+// {
+//  name : String,
+//  path : String,
+//  width : Integer @optional,
+//  height : Integer @optional
+//  frames : Integer @optional}
+//
+ToeFu.ASSETS = {
+  //asset path must be a valid game.Load.[x] method
+  SPRITESHEET : {
+    PLAYER : {
+      name : 'player',
+      path : 'assets/graphics/players_100x96.png',
+      width : 100,
+      height : 96,
+      frames : 18,
+      frames_per_row : 9 //split spritesheet by player/row
+    }
+  }
+};
--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -11,6 +11,20 @@ ToeFu.Boot = function() {
 
 ToeFu.Boot.prototype.preLoad = function() {
 // will preload all assets
+// to autoload each asset by type
+
+  Object.keys(ToeFu.ASSETS).forEach(function(type) {
+    for( var asset in ToeFu.ASSETS[type]) {
+      ToeFu.game.load[ type.toLowerCase() ] (
+       ToeFu.ASSETS[ type ][ asset ].name,
+       ToeFu.ASSETS[ type ][ asset ].path,
+       ToeFu.ASSETS[ type ][ asset ].width,
+       ToeFu.ASSETS[ type ][ asset ].height,
+       ToeFu.ASSETS[ type ][ asset ].frames
+      );
+
+    }
+  });
 };
 //these are like callbacks...once preload runs create will run
 ToeFu.Boot.prototype.create = function() {
