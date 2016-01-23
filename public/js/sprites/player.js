baf5d349d8b4f51db6b6e152a54b7fbef4368362
BBishop <brad.bishop68@gmail.com>
2016-01-23 12:39:56 -1000

    Adds first animation to player

--- public/js/sprites/player.js
+++ public/js/sprites/player.js
@@ -1,19 +1,37 @@
-//sprite class constructor
-//
-ToeFu.Player = function ( game, id, name) {
-  this.game = game;
-  this.id = id;
-  //? means if name is not defined then assign 'Player '
-  this.name = name? name : 'Player ' + (id + 1);
+(function () {
 
-  //super constructor call
-  Phaser.Sprite.call(this, game, 0, 0, ToeFu.ASSETS.SPRITESHEET.PLAYER.name);
-
-};
-
-//extend Sprite prototype
-ToeFu.Player.prototype = Object.create(Phaser.Sprite.prototype, {
-  constructor : {
-    value : ToeFu.Player
+//private static variable
+var ANIMATIONS = {
+  IDLE : {
+    name : 'idle',
+    frames : [0,1,2,3],
+    fps : 5
   }
-});
+};
+//sprite class constructor
+//@id is  0 index based
+  ToeFu.Player = function ( game, id, name) {
+    this.game = game;
+    this.id = id;
+    //? means if name is not defined then assign 'Player '
+    this.name = name? name : 'Player ' + (id + 1);
+
+    //super constructor call
+    Phaser.Sprite.call(this, game, 0, 0, ToeFu.ASSETS.SPRITESHEET.PLAYER.name);
+
+    //set animations
+    this.animations.add(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.frames );
+
+    //play the initial animation
+    this.animations.play(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.fps, true);
+
+  };
+
+  //extend Sprite prototype
+  ToeFu.Player.prototype = Object.create(Phaser.Sprite.prototype, {
+    constructor : {
+      value : ToeFu.Player
+    }
+  });
+
+})();
